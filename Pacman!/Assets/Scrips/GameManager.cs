53a3d784f0cab518475b6fdf5e195aaa74840735
sergiorvs <segio.rolando@gmail.com>
2019-10-17 11:04:29 -1000

    added score

--- Pacman!/Assets/Scrips/GameManager.cs
+++ Pacman!/Assets/Scrips/GameManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject food;
+    private GameObject pacman;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pacman = GameObject.Find("pacman");
+        time = 0.0f;
     }
 
     // Update is called once per frame
@@ -19,6 +22,14 @@ public class GameManager : MonoBehaviour
         if(food.transform.childCount == 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if(pacman == null && time > 3.5f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            time += Time.deltaTime;
         }
     }
 }
--- Pacman!/Assets/Scrips/PacDot.cs
+++ Pacman!/Assets/Scrips/PacDot.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PacDot : MonoBehaviour
 {
+    private GameObject score;
+    private int scr;
+
+    private void Start()
+    {
+        score = GameObject.Find("scr");
+    }
     private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.name == "pacman")
         {
             Destroy(gameObject);
             FindObjectOfType<AudioManager>().Play("wakawaka");
+            scr = int.Parse(score.GetComponent<TextMesh>().text) + 10;
+            score.GetComponent<TextMesh>().text = scr.ToString();
         }
     }
 }
